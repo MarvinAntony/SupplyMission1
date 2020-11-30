8231ea8cd33f43070fd23cbd13c6adde589c5b2c
MarvinAntony <73742973+MarvinAntony@users.noreply.github.com>
2020-11-30 16:29:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 var helicopterImg, helicopter, package,packageImg;
-var packageBody,ground
+var packageBody,ground;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 function preload() {
-	helicopterImg=loadImage("helicopter.png")
-	packageImg=loadImage("package.png")
+	helicopterImg=loadImage("helicopter.png");
+	packageImg=loadImage("package.png");
 }
 
 function setup() {
@@ -15,15 +15,15 @@ function setup() {
 	rectMode(CENTER);
 
 	package=createSprite(width/2, 80, 10,10);
-	package.addImage(packageIMG)
-	package.scale=0.2
+	package.addImage(packageIMG);
+	package.scale=0.2;
 
 	helicopter=createSprite(width/2, 200, 10,10);
-	helicopter.addImage(helicopterIMG)
-	helicopter.scale=0.6
+	helicopter.addImage(helicopterImg);
+	helicopter.scale=0.6;
 
 	ground=createSprite(width/2, height-35, width,10);
-	ground.shapeColor=color("white")
+	ground.shapeColor=color("white");
 
 	engine = Engine.create();
 	world = engine.world;
